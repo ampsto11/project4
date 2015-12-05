d00fe79d4845a42287c38abdd65586c7422c5980
Alex Palma <ampsto11@gmail.com>
2015-12-05 08:58:28 -0800

    pizza size bug in for loop
    optimized the for loop in the resizePizza function

--- views/js/main.js
+++ views/js/main.js
@@ -450,10 +450,12 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
       var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
       var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+      document.pizzaCount = document.querySelectorAll(".randomPizzaContainer").length;
+    
+    for (var i = 0; i < pizzaCount; i++){
+      document.getElementByClassName("randomPizzaContainer")[i].style.width = newWidth;
     }
   }
 
@@ -503,8 +505,9 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  var pageScrollTop = document.body.scrollTop;
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin((pageScrollTop/1250) + (i % 5);
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
